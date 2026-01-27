cf07a8e4137b09a3c3d7d58fd780ab80839880c5
Jude Abatangelo <abatangelo@chapman.edu>
2026-01-27 15:54:06 -0800

    another

--- src/main/java/com/example/finalprojectmod/FinalProjectMod.java
+++ src/main/java/com/example/finalprojectmod/FinalProjectMod.java
@@ -35,7 +35,7 @@ import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
-// teste comment omre test
+// teste comment omre test more test
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(FinalProjectMod.MODID)
 public class FinalProjectMod {
